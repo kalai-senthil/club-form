85f8e646596ba64ff5e1ba3c46f3d37dafd3ef8c
kalai-senthil <kalaiarasan70810@gmail.com>
2023-10-21 12:30:55 +0530

    layout-db

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,7 +4,7 @@ import type { RequestHandler, JSONObject } from "@builder.io/qwik-city";
 
 import { routeAction$ } from "@builder.io/qwik-city";
 import { collection, doc, getDocs, setDoc } from "firebase/firestore";
-import { db } from "~/services/firebase";
+// import { db } from "~/services/firebase";
 import type { Domain } from ".";
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -32,6 +32,25 @@ export default component$(() => {
     </>
   );
 });
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWkj9Hqhgvk9wfOS2Op4WPQKpLvLneFg8",
+  authDomain: "fifth-bonbon-378112.firebaseapp.com",
+  projectId: "fifth-bonbon-378112",
+  storageBucket: "fifth-bonbon-378112.appspot.com",
+  messagingSenderId: "637485073415",
+  appId: "1:637485073415:web:581a795e342dd6e87516a2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export const useGetDomains = routeLoader$(async () => {
   const domainsDocs = await getDocs(collection(db, "domainNames"));
